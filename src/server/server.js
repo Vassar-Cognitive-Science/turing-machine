79ce3fff6c8be00ee1d71ba63b074768772bfb5b
GavinQ1 <juqi@vassar.edu>
2017-04-09 15:52:17 -0400

    Record issue

--- src/server/server.js
+++ src/server/server.js
@@ -99,7 +99,7 @@ app.all('*', function(req, res) {
   res.redirect('/error/404');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
 	// var host = server.address().address;
 	var port = server.address().port;
 
