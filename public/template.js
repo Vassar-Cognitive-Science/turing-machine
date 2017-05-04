6bf9f21d0f9292a9b705f291b28243ed159816d4
GavinQ1 <juqi@vassar.edu>
2017-05-04 11:18:45 -0400

    Fix font

--- public/template.js
+++ public/template.js
@@ -12,6 +12,8 @@ export default function(preloadedState) {
 	    <link rel="stylesheet" href="./container.css">
 	    <link rel="stylesheet" href="./tape.css">
 	    <link rel="stylesheet" href="./table.css">
+	    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	    <link href="https://fonts.googleapis.com/css?family=Roboto+Mono" rel="stylesheet">
 	    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
 	    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 	    <script src="./polyfill.js"></script>
